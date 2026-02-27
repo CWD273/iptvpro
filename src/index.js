645223588b11c98a16b63329da92a08f00a95aa0
CWD273 <itscwd273@gmail.com>
2026-02-27 01:21:13 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { router } from './router.js';
 import { SessionState } from './durable/session.js';
 export default {
   async fetch(request, env, ctx) {
-    env.SESSION_STATE = env.SESSION_STATE || SessionState;
     return router.handle(request, env, ctx);
   }
 };
+export { SessionState };
